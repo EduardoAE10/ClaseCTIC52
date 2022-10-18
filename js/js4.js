7b58dcf4917f15513e4a5897c492174069b36a0b
unknown <eduardoisrael2203@gmail.com>
2022-10-18 18:59:44 -0500

    clase js4

---
+++ js/js4.js
@@ -0,0 +1,37 @@
+
+function divisores(numero){
+    let total = 0;
+    
+    for(let i=1; i < numero; i ++){
+        if(numero % 1 == 0){
+            total++;
+        }
+    }
+    return total;
+}
+
+const divisoresV2 = function(numero){
+    let total = 0;
+    
+    for(let i=1; i < numero; i ++){
+        if(numero % 1 == 0){
+            total++;
+        }
+    }
+    return total;
+}
+
+const divisoresV3 = numero =>{
+    let total = 0;
+    
+    for(let i=1; i < numero; i ++){
+        if(numero % 1 == 0){
+            total++;
+        }
+    }
+    return total;
+}
+
+console.log("Total de  divisores", divisores(32));
+console.log("Total de  divisores", divisoresV2(32));
+console.log("Total de  divisores", divisoresV3(32));
