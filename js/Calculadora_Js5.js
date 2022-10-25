2154d2377202a8f7de971d7607fd1901a5044233
unknown <eduardoisrael2203@gmail.com>
2022-10-25 16:25:07 -0500

    modificacion de css

--- js/Calculadora_Js5.js
+++ js/Calculadora_Js5.js
@@ -1,23 +1,30 @@
-function calcular() {
-    let num1 = parseInt(document.getElementById("num1").value);
-    let num2 = parseInt(document.getElementById("num2").value);
-    let operacion = (document.getElementById("operacion").value);
+function calcular(){
+    let numero1 = parseInt(document.getElementById("num1").value);
+    let numero2 = parseInt(document.getElementById("num2").value);
+    let operacion = document.getElementById("operacion").value; 
     let resultado = document.getElementById("resultado");
-    switch (operacion) {
-        case "+":
-            resultado.value = (num1+num2);
-            break;
-        case "-":
-            resultado.value = (num1-num2);
-            break;
-        case "*":
-            resultado.value = (num1*num2);
-            break;
-        case "/":
-            resultado.value = (num1/num2);
-            break;
-        default:
-            resultado.value = ("Coloque un signo");
-            break;
+
+    if(operacion == '+'){
+        resultado.value = numero1+numero2;
+    }else if(operacion == "-"){
+        resultado.value = numero1-numero2;
+    }else if(operacion == "/"){
+        resultado.value = numero1/numero2;
+    }else if(operacion == "*"){
+        resultado.value = numero1*numero2;
+    }
+
+}
+
+
+
+
+function ImpTabla() {
+    let numero = parseInt(document.getElementById("Tabla").value);
+    let lista_tabla  = document.getElementById("tabla");
+    let longitud = parseInt(document.getElementById("long").value);
+    lista_tabla.innerHTML = "";
+    for (let index = 1; index <= longitud; index++) {
+        lista_tabla.innerHTML += `<li>${numero} x ${index} = ${numero * index}</li>`;
     }
 }
